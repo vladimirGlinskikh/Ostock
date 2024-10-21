7ebf7762382bcbd6cf9f33d549bf7846cca1ff7e
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2024-10-21 12:14:45 +0300

    Start project

---
+++ src/main/java/kz/zhelezyaka/licensing_service/LicensingServiceApplication.java
@@ -0,0 +1,12 @@
+package kz.zhelezyaka.licensing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicensingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicensingServiceApplication.class, args);
+	}
+}
