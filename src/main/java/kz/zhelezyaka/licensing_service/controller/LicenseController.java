5031acd26edf3ac55a9b72905b90e60bc683beab
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2024-10-24 17:35:51 +0300

    Endpoint Realizations

---
+++ src/main/java/kz/zhelezyaka/licensing_service/controller/LicenseController.java
@@ -0,0 +1,44 @@
+package kz.zhelezyaka.licensing_service.controller;
+
+import kz.zhelezyaka.licensing_service.model.License;
+import kz.zhelezyaka.licensing_service.service.LicenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/v1/organization/{organizationId}/license")
+public class LicenseController {
+
+    @Autowired
+    private LicenseService licenseService;
+
+    @RequestMapping(value="/{licenseId}",method = RequestMethod.GET)
+    public ResponseEntity<License> getLicense(
+            @PathVariable("organizationId") String organizationId,
+            @PathVariable("licenseId") String licenseId) {
+        License license = licenseService.getLicense(licenseId, organizationId);
+        return ResponseEntity.ok(license);
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateLicense(
+            @PathVariable("organizationId") String organizationId,
+            @RequestBody License request) {
+        return ResponseEntity.ok(licenseService.updateLicense(request, organizationId));
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createLicense(
+            @PathVariable("organizationId") String organizationId,
+            @RequestBody License request) {
+        return ResponseEntity.ok(licenseService.createLicense(request, organizationId));
+    }
+
+    @DeleteMapping(value = "/{licenseId}")
+    public ResponseEntity<String> deleteLicense(
+            @PathVariable("organizationId") String organizationId,
+            @PathVariable("licenseId") String licenseId) {
+        return ResponseEntity.ok(licenseService.deleteLicense(licenseId, organizationId));
+    }
+}
