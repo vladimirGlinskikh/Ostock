49a5a249d7843a03f979d75ddb1ac7d3bf676e38
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2024-10-25 17:49:32 +0300

    Adding internationalization

--- src/main/java/kz/zhelezyaka/licensing_service/model/License.java
+++ src/main/java/kz/zhelezyaka/licensing_service/model/License.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.Locale;
+
 @Getter
 @Setter
 @ToString
--- src/main/java/kz/zhelezyaka/licensing_service/service/LicenseService.java
+++ src/main/java/kz/zhelezyaka/licensing_service/service/LicenseService.java
@@ -1,8 +1,12 @@
 package kz.zhelezyaka.licensing_service.service;
 
 import kz.zhelezyaka.licensing_service.model.License;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
+import java.util.Locale;
 import java.util.Random;
 
 @Service
@@ -18,12 +22,16 @@ public class LicenseService {
         return license;
     }
 
-    public String createLicense(License license, String organizationId) {
+//    @Qualifier("messageSource")
+    @Autowired
+    MessageSource messages;
+
+    public String createLicense(License license, String organizationId, Locale locale) {
         String responseMessage = null;
         if (license != null) {
             license.setOrganizationId(organizationId);
             responseMessage = String.format(
-                    "This is the post and the object is: %s",
+                    messages.getMessage("license.create.message", null, locale),
                     license);
         }
         return responseMessage;
@@ -34,7 +42,7 @@ public class LicenseService {
         if (license != null) {
             license.setOrganizationId(organizationId);
             responseMessage = String.format(
-                    "This is the put and object is: %s",
+                    messages.getMessage("license.update.message", null, null),
                     license);
         }
         return responseMessage;
@@ -43,7 +51,7 @@ public class LicenseService {
     public String deleteLicense(String licenseId, String organizationId) {
         String responseMessage;
         responseMessage = String.format(
-                "Deleting license with id %s for the organization %s",
+                messages.getMessage("license.delete.message", null, null),
                 licenseId, organizationId);
         return responseMessage;
     }
